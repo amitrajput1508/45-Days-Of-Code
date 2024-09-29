c2a6b06314ce8305f1958f246db846154535534c
amitrajput1508 <143792761+amitrajput1508@users.noreply.github.com>
2024-09-29 08:51:18 +0530

    Add files via upload

---
+++ Day 1/Easylevel.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        if (x >= 12) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}

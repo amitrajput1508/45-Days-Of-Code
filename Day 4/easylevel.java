1968fbe93a6425cfb12b8c976c732388a4b0fa51
amitrajput1508 <143792761+amitrajput1508@users.noreply.github.com>
2024-10-02 21:20:30 +0530

    Update easylevel.java

---
+++ Day 4/easylevel.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Solution{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int X = sc.nextInt();
+        
+        if(X==A || X==B || X==C){
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+}

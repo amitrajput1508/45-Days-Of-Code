6f9fdab15f19fd1faf914c91f1e4daef92a7e56a
amitrajput1508 <143792761+amitrajput1508@users.noreply.github.com>
2024-10-13 18:16:41 +0530

    Create hardlevel.java

---
+++ Day 16/hardlevel.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 2;
+
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        reverse(nums, i + 1, n - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+
+        int[] nums = new int[size];
+
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < size; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        Solution solution = new Solution();
+        solution.nextPermutation(nums);
+
+        System.out.println("Next permutation:");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}

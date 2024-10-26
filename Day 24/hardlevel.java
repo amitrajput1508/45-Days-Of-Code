060296d39a671a06f0db0e19a5a0df7ef72ac489
amitrajput1508 <143792761+amitrajput1508@users.noreply.github.com>
2024-10-26 20:57:29 +0530

    Create hardlevel.java

---
+++ Day 24/hardlevel.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        
+        Solution solution = new Solution();
+        System.out.println("The single number is: " + solution.singleNumber(nums));
+        
+        scanner.close();
+    }
+}

9550381293596c18f935c1fc4d53c9a51463747c
amitrajput1508 <143792761+amitrajput1508@users.noreply.github.com>
2024-10-29 20:15:09 +0530

    Create easylevel.java

---
+++ Day 27/easylevel.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PrimalityTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int N = scanner.nextInt();
+            if (isPrime(N)) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) return false;
+        }
+        return true;
+    }
+}

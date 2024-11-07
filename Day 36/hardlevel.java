5b6acb98ddaabe7838cbbcbbb237ef8c3d381d0b
amitrajput1508 <143792761+amitrajput1508@users.noreply.github.com>
2024-11-07 20:41:04 +0530

    Create hardlevel.java

---
+++ Day 36/hardlevel.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Solution {
+    public int myAtoi(String s) {
+        int i = 0, sign = 1, result = 0;
+        int n = s.length();
+        int INT_MAX = Integer.MAX_VALUE;
+        int INT_MIN = Integer.MIN_VALUE;
+
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        if (i < n && (s.charAt(i) == '-' || s.charAt(i) == '+')) {
+            sign = (s.charAt(i) == '-') ? -1 : 1;
+            i++;
+        }
+
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+
+            if (result > (INT_MAX - digit) / 10) {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+
+            result = result * 10 + digit;
+            i++;
+        }
+
+        return result * sign;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string to convert:");
+        String input = scanner.nextLine();
+        Solution solution = new Solution();
+        System.out.println("Converted integer: " + solution.myAtoi(input));
+        scanner.close();
+    }
+}

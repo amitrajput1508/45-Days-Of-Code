fea2e08ac65f0f25a53576b74ba62eedd7d356c4
amitrajput1508 <143792761+amitrajput1508@users.noreply.github.com>
2024-11-07 20:28:25 +0530

    Create easylevel.java

---
+++ Day 36/easylevel.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class DraculaEats {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int N = scanner.nextInt();
+            int count = (N >= 2) ? (N - 2) / 7 + 1 : 0;
+            System.out.println(count);
+        }
+        scanner.close();
+    }
+}

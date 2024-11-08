7db400d7663505b0ce7fb2db70a520c53242a180
amitrajput1508 <143792761+amitrajput1508@users.noreply.github.com>
2024-11-08 19:35:27 +0530

    Create hardlevel.java

---
+++ Day 37/hardlevel.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        TreeNode root = sortedArrayToBST(nums);
+        preOrder(root);
+        sc.close();
+    }
+
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    private static TreeNode helper(int[] nums, int left, int right) {
+        if (left > right) return null;
+        int mid = left + (right - left) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = helper(nums, left, mid - 1);
+        node.right = helper(nums, mid + 1, right);
+        return node;
+    }
+
+    public static void preOrder(TreeNode root) {
+        if (root != null) {
+            System.out.print(root.val + " ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int x) { val = x; }
+}
